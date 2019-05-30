616da849ad0155563537b8c3b28c439ea0810eef
fko <felix.kopf@ibs.valantic.com>
2019-05-30 17:24:35 +0200

    Missed things in initial commit

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,7 @@ import express from 'express';
 import {NextFunction, Request, Response} from 'express-serve-static-core';
 import session from 'express-session';
 import expressValidator from 'express-validator';
-
 import createError from 'http-errors';
-import sassMiddleware from 'node-sass-middleware';
-import path from 'path';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
@@ -19,10 +16,6 @@ const MemoryStore = (require('memorystore'))(session);
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-
-// view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'pug');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -36,16 +29,6 @@ app.use(session({
         checkPeriod: 3600000 // prune expired entries every hour
     })
 }));
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true
-}));
-
-app.use(
-    express.static(path.join(__dirname, 'public'), {maxAge: 31557600000})
-);
 
 if (app.get('env') === 'development') {
     app.use(errorhandler({log: logger.error}));
@@ -74,6 +57,6 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.render('error');
 });
 
-logger.log('info', 'Started app.');
+logger.log('info', 'Started backend.');
 
 export default app;
